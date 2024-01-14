7231a07d51e646bbf010e1403bd3c7fce78ac13a
Islam Ahmed <eslamelbeltagye1111@gmail.com>
2024-01-14 07:06:52 +0200

    test_palce

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+
+import unittest
+from models.base_model import BaseModel
+from models.place import Place
+from datetime import datetime
+
+class TestPlace(unittest.TestCase):
+
+    def setUp(self):
+        self.place = Place()
+
+    def test_class_exists(self):
+        """Test if class exists"""
+        self.assertEqual(str(type(self.place)), "<class 'models.place.Place'>")
+
+    def test_user_inheritance(self):
+        """Test if Place is a subclass of BaseModel"""
+        self.assertIsInstance(self.place, BaseModel)
+        self.assertTrue(hasattr(self.place, "id"))
+        self.assertTrue(hasattr(self.place, "created_at"))
+        self.assertTrue(hasattr(self.place, "updated_at"))
+
+    def test_instance(self):
+        """Test for an instance of Place"""
+        self.assertIsInstance(self.place, Place)
+
+    def test_attributes(self):
+        """Test if attributes are correctly set"""
+        self.assertEqual(self.place.city_id, "")
+        self.assertEqual(self.place.user_id, "")
+        self.assertEqual(self.place.name, "")
+        self.assertEqual(self.place.description, "")
+        self.assertEqual(self.place.number_rooms, 0)
+        self.assertEqual(self.place.number_bathrooms, 0)
+        self.assertEqual(self.place.max_guest, 0)
+        self.assertEqual(self.place.price_by_night, 0)
+        self.assertEqual(self.place.latitude, 0.0)
+        self.assertEqual(self.place.longitude, 0.0)
+        self.assertEqual(self.place.amenity_ids, [])
+
+    def test_attribute_types(self):
+        """Test if attributes are of correct type"""
+        self.assertIsInstance(self.place.city_id, str)
+        self.assertIsInstance(self.place.user_id, str)
+        self.assertIsInstance(self.place.name, str)
+        self.assertIsInstance(self.place.description, str)
+        self.assertIsInstance(self.place.number_rooms, int)
+        self.assertIsInstance(self.place.number_bathrooms, int)
+        self.assertIsInstance(self.place.max_guest, int)
+        self.assertIsInstance(self.place.price_by_night, int)
+        self.assertIsInstance(self.place.latitude, float)
+        self.assertIsInstance(self.place.longitude, float)
+        self.assertIsInstance(self.place.amenity_ids, list)
+
+    def test_no_args_instantiates(self):
+        """Test that no arguments instantiates Place"""
+        self.assertEqual(Place, type(Place()))
+
+    def test_id_is_public_str(self):
+        """Test if id is public and is a string"""
+        self.assertEqual(str, type(self.place.id))
+
+    def test_updated_at_is_public_datetime(self):
+        """Test if updated_at is public and is a datetime"""
+        self.assertEqual(datetime, type(self.place.updated_at))
+
+    def test_created_at_is_public_datetime(self):
+        """Test if created_at is public and is a datetime"""
+        self.assertEqual(datetime, type(self.place.created_at))
+
+    def test_str_method(self):
+        """Test if the str method has the correct output"""
+        string = "[Place] ({}) {}".format(self.place.id, self.place.__dict__)
+        self.assertEqual(string, str(self.place))
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_models/test_state.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+# FILEPATH: /d:/ALX/Python/projects/Airbnb-The consol/test-bnb/tests/test_models/test_state.py
+
+import unittest
+from models.state import State
+from models.base_model import BaseModel
+
+class TestState(unittest.TestCase):
+    """Test the State class"""
+
+    def setUp(self):
+        """Set up for the tests"""
+        self.my_state = State()
+
+    def tearDown(self):
+        """Tear down for the tests"""
+        del self.my_state
+
+    def test_is_instance_base_model(self):
+        """Test if my_state is an instance of BaseModel"""
+        self.assertIsInstance(self.my_state, BaseModel)
+
+    def test_field_name(self):
+        """Test the name field of State"""
+        self.assertEqual(type(self.my_state.name), str)
+        self.assertEqual(self.my_state.name, "")
+
+    def test_field_name_assignment(self):
+        """Test assigning value to name field of State"""
+        self.my_state.name = "California"
+        self.assertEqual(self.my_state.name, "California")
+
+    def test_to_dict(self):
+        """Test the to_dict method of State"""
+        state_dict = self.my_state.to_dict()
+        self.assertEqual(type(state_dict), dict)
+        self.assertFalse('name' in state_dict)
+        self.assertTrue('__class__' in state_dict)
+        self.assertEqual(state_dict['__class__'], 'State')
+
+    def test_class_exist(self):
+        """test for test class exist"""
+        state = State()
+        test = "<class 'models.state.State'>"
+        self.assertEqual(str(type(state)), test)
+
+    def test_str(self):
+        """Test the __str__ method of State"""
+        state_str = str(self.my_state)
+        self.assertEqual(type(state_str), str)
+        self.assertTrue("State" in state_str)
+
+if __name__ == "__main__":
+    unittest.main()
