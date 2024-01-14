ed5cc39007966fdf525430383e5b0dc623925f7d
wessam18 <wessamabdelnabi903@gmail.com>
2024-01-14 05:36:46 +0200

    base model test cases

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,15 +1,8 @@
-#!/usr/bin/python3
-"""test cases to test the base model class"""
-
-
 import unittest
 import pycodestyle
 import uuid
 import models
 import inspect
-import os
-import time
-
 from datetime import datetime, timedelta
 from models.base_model import BaseModel
 from models.state import State
@@ -18,35 +11,29 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
-
-class Test_base_model(unittest.TestCase):
-    """the main class to test"""
+class TestBaseModel(unittest.TestCase):
+    def setUp(self):
+        self.model = BaseModel()
 
     def test_base_model(self):
-        """ check for class instance"""
         my_obj = BaseModel()
         self.assertIsInstance(my_obj, BaseModel)
 
     def test_created_at_type(self):
-        """ test for created at type """
         self.assertEqual(type(self.model.created_at), datetime)
 
     def test_updated_at_type(self):
-        """ test for updated at type """
         self.assertEqual(type(self.model.updated_at), datetime)
 
     def test_basemodel_id(self):
-        """ check if id is unique """
         my_Id1 = BaseModel()
         my_Id2 = BaseModel()
         self.assertNotEqual(my_Id1.id, my_Id2.id)
 
     def test_id_type(self):
-        """ test for id type """
         self.assertEqual(type(self.model.id), str)
 
     def test_basemodel_str(self):
-        """check if the output is str"""
         my_str = BaseModel()
         my_dict = my_str.__dict__
         str1 = f"[BaseModel] ({my_str.id}) {my_dict}"
@@ -54,39 +41,31 @@ class Test_base_model(unittest.TestCase):
         self.assertEqual(str1, str2)
 
     def test_basemodel_str_method(self):
-        bm = BaseModel()
-        str_representation = str(bm)
+        str_representation = str(self.model)
         self.assertIsInstance(str_representation, str)
-        self.assertIn(bm.id, str_representation)
-        self.assertIn(str(bm.__class__.__name__), str_representation)
+        self.assertIn(self.model.id, str_representation)
+        self.assertIn(str(self.model.__class__.__name__), str_representation)
 
     def test_basemodel_save(self):
-        """ check if date update when save """
-        my_time = BaseModel()
-        first_time = my_time.updated_at
-        my_time.save()
-        second_time = my_time.updated_at
+        first_time = self.model.updated_at
+        self.model.save()
+        second_time = self.model.updated_at
         self.assertNotEqual(first_time, second_time)
 
     def test_basemodel_pycodestyle(self):
-        """check pycodestyle"""
         betty = pycodestyle.StyleGuide(ignore=['E501', 'W503'])
         my_path = "models/base_model.py"
         res = betty.check_files([my_path])
         self.assertEqual(res.total_errors, 0)
 
     def test_basemodel_doc(self):
-        """check for the decoumentation"""
-        self.assertIsNotNone(BaseModel.__doc__,
-            'no docs for BaseModel Class')
-        self.assertIsNotNone(inspect.getdoc(models.base_model),\
-            'no docs for module')
+        self.assertIsNotNone(BaseModel.__doc__, 'no docs for BaseModel Class')
+        self.assertIsNotNone(inspect.getdoc(models.base_model), 'no docs for module')
         for name, method in inspect.getmembers(BaseModel, inspect.isfunction):
             self.assertIsNotNone(method.__doc__, f"{name} has no docs")
 
     def test_to_dict_method(self):
-        bm = BaseModel()
-        obj_dict = bm.to_dict()
+        obj_dict = self.model.to_dict()
         self.assertIsInstance(obj_dict, dict)
         self.assertIn('id', obj_dict)
         self.assertIn('created_at', obj_dict)
@@ -96,14 +75,13 @@ class Test_base_model(unittest.TestCase):
     def test_invalid_initialization(self):
         invalid_dict = {"id": "id-32", "created_at": "today", "updated_at": "1111102223", "__class__": "abc"}
         with self.assertRaises(ValueError):
-            bm = BaseModel(**invalid_dict)
+            BaseModel(**invalid_dict)
 
     def test_default_instance(self):
-        bm = BaseModel()
-        self.assertIsInstance(bm, BaseModel)
-        self.assertIsInstance(bm.id, str)
-        self.assertIsInstance(bm.created_at, datetime)
-        self.assertIsInstance(bm.updated_at, datetime)
+        self.assertIsInstance(self.model, BaseModel)
+        self.assertIsInstance(self.model.id, str)
+        self.assertIsInstance(self.model.created_at, datetime)
+        self.assertIsInstance(self.model.updated_at, datetime)
 
     def test_instance_with_kwargs(self):
         valid_dict = {"id": "id-32", "created_at": "2024-01-10T22:40:45.795104", "updated_at": "2024-01-10T22:40:45.795104", "__class__": "BaseModel"}
