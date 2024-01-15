d72c590f8e24623c107c5e990d9645bff70d7a1d
wessam18 <wessamabdelnabi903@gmail.com>
2024-01-15 03:14:43 +0200

    test cases console

--- tests/test_console.py
+++ tests/test_console.py
@@ -76,6 +76,35 @@ class TestConsole(unittest.TestCase):
             output,
             '^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$')
 
+    def test_do_update_command(self):
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.console.onecmd('create User')
+            instance_id = output.getvalue().strip()
+
+            self.console.onecmd(f'update User {instance_id} age 25')
+            self.console.onecmd(f'show User {instance_id}')
+
+            # Verify that the 'age' attribute has been updated
+            self.assertIn("'age': 25", output.getvalue().strip())
+
+    def test_help_update(self):
+        h = ("Usage: update <class> <id> <attribute_name> <attribute_value> or"
+             "\n       <class>.update(<id>, <attribute_name>, <attribute_value"
+             ">) or\n       <class>.update(<id>, <dictionary>)\n        "
+             "Update a class instance of a given id by adding or updating\n   "
+             "     a given attribute key/value pair or dictionary.")
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(self.console.onecmd("help update"))
+            self.assertEqual(h, output.getvalue().strip())
+
+    def test_prompt_string(self):
+        self.assertEqual("(hbnb) ", HBNBCommand.prompt)
+
+    def test_empty_line(self):
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd(""))
+            self.assertEqual("", output.getvalue().strip())
+
 
 if __name__ == '__main__':
     unittest.main()
