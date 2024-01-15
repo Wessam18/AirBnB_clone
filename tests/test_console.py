c0d98ec4dc36909b66298210f8929a3f2913ea14
wessam18 <wessamabdelnabi903@gmail.com>
2024-01-15 02:35:32 +0200

    console test cases

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""test cases for the console"""
+"""test cases to test the base model class"""
 
 import unittest
 from unittest.mock import patch
@@ -8,94 +8,73 @@ from console import HBNBCommand
 
 
 class TestConsole(unittest.TestCase):
-    """class test the console"""
+    """Class to test the console"""
+
     def setUp(self):
         """Set up for the tests"""
-        self.cli = HBNBCommand()
+        self.console = HBNBCommand()
 
     def tearDown(self):
         """Tear down for the tests"""
         pass
 
-    def test_do_quit(self):
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_do_quit(self, mock_stdout):
         """Test the do_quit command"""
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("quit")
-            self.assertEqual(f.getvalue(), '')
+        self.console.onecmd("quit")
+        self.assertEqual(mock_stdout.getvalue(), '')
 
-    def test_do_EOF(self):
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_do_EOF(self, mock_stdout):
         """Test the do_EOF command"""
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("EOF")
-            self.assertEqual(f.getvalue(), '\n')
+        self.console.onecmd("EOF")
+        self.assertEqual(mock_stdout.getvalue(), '\n')
 
-    def test_emptyline(self):
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_emptyline(self, mock_stdout):
         """Test the emptyline command"""
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("\n")
-            self.assertEqual(f.getvalue(), '')
+        self.console.onecmd("\n")
+        self.assertEqual(mock_stdout.getvalue(), '')
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_do_all(self, mock_stdout):
-        """Test do all"""
-        self.cli.do_all('')
-        self.assertEqual(mock_stdout.getvalue(), "[]\n")
-        self.cli.do_all('User')
-        self.assertEqual(mock_stdout.getvalue(), "[]\n")
+        """Test do_all command"""
+        self.console.onecmd('all User')
+        self.assertEqual(mock_stdout.getvalue(), '[]\n')
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_do_count(self, mock_stdout):
-        """Test do all"""
-        self.cli.do_count('User')
-        self.assertEqual(mock_stdout.getvalue(), "0\n")
-
-    def test_precmd(self):
-        """Test do all"""
-        self.assertEqual(
-                self.cli.precmd('User.create("1234")'), 'create User 1234')
+        """Test do_count command"""
+        self.console.onecmd('count User')
+        self.assertEqual(mock_stdout.getvalue(), '0\n')
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_do_show(self, mock_stdout):
-        """Test do show"""
-        self.cli.do_show('')
+        """Test do_show command"""
+        self.console.onecmd('show')
         self.assertEqual(mock_stdout.getvalue(), "** class name missing **\n")
-        self.cli.do_show('User')
-        self.assertEqual(mock_stdout.getvalue(), "** instance id missing **\n")
-        self.cli.do_show('User 1234')
-        self.assertEqual(mock_stdout.getvalue(), "** no instance found **\n")
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_do_update(self, mock_stdout):
-        """Test do update"""
-        self.cli.do_update('')
+        """Test do_update command"""
+        self.console.onecmd('update')
         self.assertEqual(mock_stdout.getvalue(), "** class name missing **\n")
-        self.cli.do_update('User')
-        self.assertEqual(mock_stdout.getvalue(), "** instance id missing **\n")
-        self.cli.do_update('User 1234')
-        self.assertEqual(mock_stdout.getvalue(), "** no instance found **\n")
-        self.cli.do_update('User 1234 name')
-        self.assertEqual(mock_stdout.getvalue(), "** value missing **\n")
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_do_destroy(self, mock_stdout):
-        """Test do destroy"""
-        self.cli.do_destroy('')
+        """Test do_destroy command"""
+        self.console.onecmd('destroy')
         self.assertEqual(mock_stdout.getvalue(), "** class name missing **\n")
-        self.cli.do_destroy('User')
-        self.assertEqual(mock_stdout.getvalue(), "** instance id missing **\n")
-        self.cli.do_destroy('User 1234')
-        self.assertEqual(mock_stdout.getvalue(), "** no instance found **\n")
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_do_create(self, mock_stdout):
-        """test create method"""
-        self.cli.do_create('')
-        self.assertEqual(mock_stdout.getvalue(), "** class name missing **\n")
-        self.cli.do_create('User')
+        """Test do_create command"""
+        self.console.onecmd('create User')
+        output = mock_stdout.getvalue().strip()
+        self.assertIsNotNone(output)
         self.assertRegex(
-            mock_stdout.getvalue(),
-            '^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\n$')
+            output,
+            '^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$')
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
